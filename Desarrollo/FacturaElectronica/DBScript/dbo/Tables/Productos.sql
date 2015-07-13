9b84167ed1257983615240b5da5191ee92d76d9a
Rodrigo <rodrigo.moralesv@hotmail.com>
2015-07-12 22:27:54 -0300

    cambios

--- Desarrollo/FacturaElectronica/DBScript/dbo/Tables/Productos.sql
+++ Desarrollo/FacturaElectronica/DBScript/dbo/Tables/Productos.sql
@@ -1,16 +1,14 @@
 ﻿CREATE TABLE [dbo].[Productos] (
     [CodProducto]            VARCHAR (35)     NOT NULL,
     [Descripcion]            VARCHAR (80)     NOT NULL,
-    [PrecioVentaNeto]        INT              NOT NULL,
+	[CodBarra]				 VARCHAR(100)	  NOT NULL,
     [ActivarPrecioMayorista] BIT              NOT NULL,
-    [Exento]                 BIT              NOT NULL,
-    [Inventariable]          BIT              NOT NULL,
-    [Descontinuado]          BIT              NOT NULL,
     [UnidadMedida]           INT              NULL,
     [Observacion]            VARCHAR (500)    NULL,
-    [DescripcionLarga]       VARCHAR (500)    NULL,
-    [CentroCostoId]          UNIQUEIDENTIFIER NULL,
-    [CodAlternativo]         VARCHAR (35)     NULL,
+    [CategoriaId]			 INT     NOT NULL,
+	[PrecioVentaNeto]		 INT NOT NULL,
+    [StockAlerta] INT NULL, 
+    [StockDeseado] INT NULL, 
     CONSTRAINT [PK_Producto] PRIMARY KEY CLUSTERED ([CodProducto] ASC)
 );
 
