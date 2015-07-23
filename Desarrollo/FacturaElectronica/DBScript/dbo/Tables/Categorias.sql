6dfaf7103ed43a239e3686f30cde6103b223f94f
Rodrigo <rodrigo.moralesv@hotmail.com>
2015-07-23 00:09:36 -0300

    changes

---
+++ Desarrollo/FacturaElectronica/DBScript/dbo/Tables/Categorias.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Categorias] (
+    [Id]     INT           IDENTITY (1, 1) NOT NULL,
+    [Nombre] VARCHAR (100) NULL,
+    [Estado] INT           NULL,
+    CONSTRAINT [PK_Categorias] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Desarrollo/FacturaElectronica/ServiceLayer/Dal/DTEContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Syspan.Core.Models;
+using Syspan.Core.Models.Generic;
+using Erp.Dte.ServiceLayer.Model;
+using Erp.Dte.ServiceLayer.Model.Clientes;
+
+namespace Erp.Dte.ServiceLayer.Dal
+{
+    public class DTEContext : DbContext
+    {
+        public DTEContext()
+            : base("connectionString")
+        {
+        }
+
+        public DbSet<ClienteInfo> Clientes { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
