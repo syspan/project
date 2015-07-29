8bd0676cf9bbb87c1ac4a69f7457a40e00a067ee
Rodrigo <rodrigom@gmed.com>
2015-07-29 16:53:47 -0300

    tool

---
+++ Testing/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(140, 37);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(116, 20);
+            this.txtTo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "To";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(44, 75);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(187, 124);
+            this.txtMessage.TabIndex = 2;
+            this.txtMessage.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Testing/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WhatsAppApi;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string from = "56966411028";
+            string to = txtTo.Text;
+            string msg = txtMessage.Text;
+
+            WhatsApp wa = new WhatsApp(from, "hXYN1vWci60z0Df6s7yautuO+Ks=", "rodrigo", false, false);
+            
+            wa.OnConnectSuccess += () =>
+            {
+                MessageBox.Show("Conectando a WhatsAppa");
+
+                wa.OnLoginSuccess += (phoneNumber, data) =>
+                {
+                    wa.SendMessage(to, msg);
+                    MessageBox.Show("Mensaje enviado");
+
+                };
+
+                wa.OnLoginFailed += (data) =>
+                {
+                    MessageBox.Show(string.Format("Fallo login : {0} ", data));
+                };
+
+                wa.Login();
+
+            };
+
+            wa.OnConnectFailed += (ex) =>
+            {
+                MessageBox.Show("Fallo la conexion");
+            };
+
+            wa.Connect();
+
+            //WIhDFaZ/gB4kEv6fk/d9h7rlhJY=
+
+        }
+    }
+}
