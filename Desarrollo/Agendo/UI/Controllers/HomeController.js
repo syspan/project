e27e920b80f50dc363710a1e76bb0e466dae801b
Rodrigo <rodrigo.moralesv@hotmail.com>
2015-10-20 01:33:26 -0200

    some changes

---
+++ Desarrollo/Agendo/UI/Controllers/HomeController.js
@@ -0,0 +1,6 @@
+﻿(function () {
+    var HomeController = function ($scope) {
+        $scope.Message = "WelCome to Online Project Tracking Web site";
+    };
+    app.controller("HomeController", HomeController);
+}());
